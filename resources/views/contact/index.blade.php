c61cb8a829fa0760d8c87e077370849778bc44e8
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-06-23 17:14:39 +0600

    fixed customer show issue

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -11,7 +11,7 @@
 @section('content')
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <h1 class="tw-text-xl md:tw-text-3xl tw-font-bold tw-text-black"> fsdfsdf
+        <h1 class="tw-text-xl md:tw-text-3xl tw-font-bold tw-text-black"> Customers
             <small class="tw-text-sm md:tw-text-base tw-text-gray-700 tw-font-semibold">@lang('contact.manage_your_contact', ['contacts' => __('lang_v1.' . $type . 's')])</small>
         </h1>
     </section>
