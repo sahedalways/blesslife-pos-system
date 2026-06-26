c91a63840ebca2a7bee3200a33f595f6feaa08ca
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-06-26 12:45:54 +0600

    hero UI modified

--- Modules/Cms/Resources/views/frontend/pages/partials/cta.blade.php
+++ Modules/Cms/Resources/views/frontend/pages/partials/cta.blade.php
@@ -8,15 +8,25 @@
                 Talk to one of our product experts. We’re here to help you get started for your business.
             </p>
         </div>
-        <div class="col-lg-4 mx-auto mt-4 mt-xl-0 d-flex flex-row-reverse align-items-center justify-content-center justify-content-lg-start">
+        <div
+             class="col-lg-4 mx-auto mt-4 mt-xl-0 d-flex flex-row-reverse align-items-center justify-content-center justify-content-lg-start">
             <div class="text-center">
-                <a class="btn btn-secondary" href="{{ $cta_btn['link'] }}">
+                <a class="btn btn-secondary"
+                   href="{{ $cta_btn['link'] }}">
                     <span class="mx-1">
-                        {{$cta_btn['text']}}
+                        {{ $cta_btn['text'] }}
                     </span>
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="mx-1 bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z"></path>
-                        <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"></path>
+                    <svg width="1em"
+                         height="1em"
+                         viewBox="0 0 16 16"
+                         class="mx-1 bi bi-arrow-right"
+                         fill="currentColor"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z">
+                        </path>
+                        <path fill-rule="evenodd"
+                              d="M2 8a.5.5 0 0 1 .5-.5H13a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8z"></path>
                     </svg>
                 </a>
             </div>
