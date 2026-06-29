5eb6c173aa930a99ff8cf7ed94af78d09025e6a0
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-06-29 16:36:57 +0600

    fix: bg slides left-to-right on hover, text stays clean

--- Modules/Superadmin/Resources/views/subscription/partials/package_card.blade.php
+++ Modules/Superadmin/Resources/views/subscription/partials/package_card.blade.php
@@ -369,26 +369,26 @@
         @if ($package->enable_custom_link == 1)
             <a href="{{ $package->custom_link }}"
                class="pps-pkg-btn bls-submit-btn">
-                <span>{{ $package->custom_link_text }}</span>
+                {{ $package->custom_link_text }}
             </a>
         @else
             @if (isset($action_type) && $action_type == 'register')
                 <a href="{{ route('business.getRegister') }}?package={{ $package->id }}"
                    class="pps-pkg-btn bls-submit-btn">
-                    <span>@if ($package->price != 0)
+                    @if ($package->price != 0)
                         @lang('superadmin::lang.register_subscribe')
                     @else
                         @lang('superadmin::lang.register_free')
-                    @endif</span>
+                    @endif
                 </a>
             @else
                 <a href="{{ action([\Modules\Superadmin\Http\Controllers\SubscriptionController::class, 'pay'], [$package->id]) }}"
                    class="pps-pkg-btn bls-submit-btn">
-                    <span>@if ($package->price != 0)
+                    @if ($package->price != 0)
                         @lang('superadmin::lang.pay_and_subscribe')
                     @else
                         @lang('superadmin::lang.subscribe')
-                    @endif</span>
+                    @endif
                 </a>
             @endif
         @endif
