a567f734f81d8066aafcf9131df7ef421313f74a
Sk. Sahed Ahmed <73462728+sahedalways@users.noreply.github.com>
2026-07-01 16:28:01 +0600

    notifications button: use premium-btn style

--- resources/views/layouts/partials/header-notifications.blade.php
+++ resources/views/layouts/partials/header-notifications.blade.php
@@ -6,12 +6,12 @@
 <!-- Notifications: style can be found in dropdown.less -->
 <li class="dropdown notifications-menu tw-list-none">
     <a type="button"
-        class="dropdown-toggle load_notifications tw-inline-flex tw-items-center tw-ring-1 tw-ring-white/10 tw-justify-center tw-text-sm tw-font-medium tw-text-white hover:tw-text-white tw-transition-all tw-duration-200 tw-bg-@if(!empty(session('business.theme_color'))){{session('business.theme_color')}}@else{{'primary'}}@endif-800 hover:tw-bg-@if(!empty(session('business.theme_color'))){{session('business.theme_color')}}@else{{'primary'}}@endif-700 tw-p-1.5 tw-rounded-lg"
+        class="premium-btn dropdown-toggle load_notifications"
         data-toggle="dropdown" id="show_unread_notifications" data-loaded="false">
         <span class="tw-sr-only">
             Notifications
         </span>
-        <svg aria-hidden="true" class="tw-size-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5"
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
